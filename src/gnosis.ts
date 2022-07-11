f00ce95bfbc6e8d0a8e1d29cc80e0fc9eb4d859b
HHK <hhk.contact@protonmail.com>
2022-07-11 18:11:45 +0200

    feat: add gnosis api fetcher function

---
+++ src/gnosis.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export async function querySafeTxs(address: string): Promise<any> {
+  const query = await axios.get(
+    'https://safe-transaction.polygon.gnosis.io/api/v1/safes/' + address + '/multisig-transactions/?limit=10'
+  );
+
+  if (query.status !== 200) return null;
+  return query.data;
+}
